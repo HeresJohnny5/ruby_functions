c39d581bee412e4502412bc1983066e5079cff89
John Erickson <ericksonjohn08@hotmail.com>
2016-08-08 10:39:21 -0400

    added second addition function

--- addition/add.rb
+++ addition/add.rb
@@ -1,3 +1,5 @@
 def my_addition(num1, num2)
-	0
+	num1 + num2
 end
+
+
--- addition/test_add.rb
+++ addition/test_add.rb
@@ -5,4 +5,8 @@ class TestAddition < Minitest::Test # require for boilerplate. The first letter 
 	def test_0_add_0_equals_0 # when you create a function you need to start w/ test (test will be lowercase)
 		assert_equal(0, my_addition(0, 0))
 	end
+
+	def test_5_add_10_equals_15
+		assert_equal(15, my_addition(5, 10))
+	end
 end
